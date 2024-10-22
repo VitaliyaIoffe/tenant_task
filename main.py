1487bec8265907d36d1276c5c7650d6e0125642b
Vitaliia Ioffe <vitaioffe@yandex.ru>
2024-10-22 18:05:45 +0300

    Improve app execution

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import argparse
 import getpass
+import logging
 
+from src.database import DatabaseManager
 from src.models import TenantPortal
+from src.portals import ClickPayPortalRetriever, PortalRetriever
 
 
 def parse_args() -> argparse.Namespace:
+    logging.info("Parsing arguments")
     parser = argparse.ArgumentParser(
         description="Retrieve tenant data from a portal and store it in a database."
     )
@@ -17,18 +21,25 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def main() -> None:
-    """Main entry point of the program."""
-    args = parse_args()
-    password = getpass.getpass(prompt="Enter your password: ")
-
-    if args.tenant_portal == TenantPortal.CLICK_PAY:
-        print(f"Retrieving data from {args.tenant_portal.value} for user {args.username}...")
-        pass
-        # print("Data successfully inserted into the database.")
+def get_portal(tenant_portal: str, username: str, password: str) -> PortalRetriever:
+    logging.info("Getting portal")
+    portal = None
+    if tenant_portal == TenantPortal.CLICK_PAY.value:
+        print(f"Retrieving data from {tenant_portal} for user {username}...")
+        portal = ClickPayPortalRetriever(TenantPortal.CLICK_PAY)
+        print("Data successfully inserted into the database.")
     else:
-        raise NotImplementedError(f"Portal '{args.tenant_portal.value}' is not supported.")
+        raise NotImplementedError(f"Portal '{args.tenant_portal}' is not supported.")
+
+    return portal
 
 
 if __name__ == "__main__":
-    main()  # Run the main program
+    logging.info("Starting app")
+    args = parse_args()
+    password = getpass.getpass(prompt="Enter your password: ")
+    database_manager = DatabaseManager()
+    portal = get_portal(tenant_portal=args.tenant_portal, username=args.username, password=password)
+    data = portal.retrieve_data()
+    database_manager.insert_tenant(data)
+    logging.info("Finished")
